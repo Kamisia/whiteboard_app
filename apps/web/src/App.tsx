43956c2c56c5fa3e74d56b5d0bbda1e809e66d87
Kamisia <kamila.samczuk@gmail.com>
2026-02-04 13:15:36 +0100

    chore: add prettier and root formatting config

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,5 @@
-
 const App = () => {
-  return (
-    <h1 className="text-2xl font-bold">Whiteboard Notes</h1>
+  return <h1 className="text-2xl font-bold">Whiteboard Notes</h1>;
+};
 
-  )
-}
-
-export default App
+export default App;
